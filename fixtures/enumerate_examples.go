81dedcee7527603353af4fdbf97180f3a32d85dd
Victor M. C. Lima <marcolino.victor@gmail.com>
2025-01-14 18:34:04 +0100

    feat(fixtures): add example enums and struct definitions
    Introduce `ExampleString` and `ExampleInt` enums along with an `Example` struct for use in testing and type generation.
    
    Signed-off-by: Victor Marcolino <victor.marcolino@gmail.com>

---
+++ fixtures/enumerate_examples.go
@@ -0,0 +1,21 @@
+package fixtures
+
+type ExampleString string
+
+const (
+	ExampleString1   ExampleString = "1"
+	ExampleStringTwo ExampleString = "2"
+	ExampleString3   ExampleString = "3"
+)
+
+type ExampleInt int
+
+const (
+	Code1   ExampleInt = 1
+	CodeTwo ExampleInt = 2
+)
+
+type Example struct {
+	ExampleString ExampleString `json:"exampleString"`
+	ExampleInt    ExampleInt    `json:"exampleInt"`
+}
